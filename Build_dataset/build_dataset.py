1672aa1a3fb9e2f932e0fbbc3a15c0f2cffa62e9
Lino Lerch <Lerch.Lino@gmail..com>
2023-07-13 17:13:47 +0200

    Improve build dataset, add using segmentation - Improve build dataset logging to indicate progress - add function to skeletonize starting from segmentation label

--- Build_dataset/build_dataset.py
+++ Build_dataset/build_dataset.py
@@ -1,12 +1,13 @@
 import os
 import glob
 import json
+import cv2
 from transformers import pipeline
 from transformers.pipelines.pt_utils import KeyDataset
 from datasets import load_dataset
-from datasets import Image as DatasetsImage
-from skimage.io import imsave, imread
+from skimage.io import imsave
 from skimage.util import img_as_ubyte
+from skimage.morphology import skeletonize
 from PIL import Image
 
 from skeletonize import skeleton_pipeline
@@ -29,6 +30,23 @@ def append_img_to_dataset(img_path, img_nr, output_folder):
 
 
 def build_dataset_from_folder(input_folder, DATASET_DIR):
+    """
+    Build a dataset from a folder with subfolders of images. \n
+    Images are skeletonized and added to the dataset along with the original image. \n
+    If the dataset folder already contains images, the new images are appended.
+
+    The input_folder structure should be as follows:
+    - input_folder
+        - folder1
+            - image1.jpg
+            - image2.jpg
+            - ...
+        - folder2
+            - image1.jpg
+            - image2.jpg
+            - ...
+    """
+
     DS_IMAGES_DIR = os.path.join(DATASET_DIR, "images")
     # Create output folder if it does not exist
     if not os.path.exists(DATASET_DIR):
@@ -37,12 +55,16 @@ def build_dataset_from_folder(input_folder, DATASET_DIR):
         os.makedirs(DS_IMAGES_DIR)
         os.makedirs(os.path.join(DATASET_DIR, "conditioning_images"))
 
-    folder_list = get_files_folder(input_folder)
+    # Get the number of images already in the dataset
+    nr_imgs = len(os.listdir(DS_IMAGES_DIR))
+    print(f'Buiding / extending dataset (current size: {nr_imgs} images)')
 
-    for folder in folder_list:
+    folder_list = get_files_folder(input_folder)
+    nr_folders = len(folder_list)
+
+    for (i,folder) in enumerate(folder_list):
         # Get all the images in the input folder
         image_list = get_files_folder(folder)
-
         next_img_nr = len(os.listdir(DS_IMAGES_DIR))
         
         # Run the skeleton pipeline for each image
@@ -54,9 +76,55 @@ def build_dataset_from_folder(input_folder, DATASET_DIR):
                 print(f'An error occurred in image {img_path}: {error}')
         
         folder_name = folder.split("\\")[-1]
-        print(f'Folder \'{folder_name}\' added to Dataset (new size {next_img_nr} images)')
+        print(f'Folder {i+1} of {nr_folders} \'{folder_name}\' added to Dataset (new size {next_img_nr} images)')
 
-    # Create jsonl data file 
+    create_jsonl_data_file(DATASET_DIR)
+
+    nr_of_images = len(os.listdir(DS_IMAGES_DIR))
+    print(f'Dataset created with {nr_of_images} images')
+
+
+def build_dataset_using_segmentation_labels(input_folder, segmentation_folder, DATASET_DIR):
+    """
+    Build a dataset from a folder of images and a folder with respective segmentation labels. \n
+    (assuming the segmentation labels have the same filename as the images)
+    """
+    
+    DS_IMAGES_DIR = os.path.join(DATASET_DIR, "images")
+    next_img_nr = len(os.listdir(DS_IMAGES_DIR))
+    print(f'Buiding / extending dataset (using segmentation labels) (current size: {next_img_nr} images)')
+    image_list = get_files_folder(input_folder)
+    
+    for img_path in image_list:
+        try:
+            # Open the image and the segmentation label	
+            input = Image.open(img_path)
+            filename = os.path.basename(img_path)
+            img = cv2.imread(os.path.join(segmentation_folder, filename), cv2.IMREAD_GRAYSCALE)
+            thresh = 1
+            im_bw = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)[1]
+            segmentation = Image.open()
+
+            skeleton = skeletonize(segmentation)
+            
+            # Save both the original image and the skeletonized image
+            output_path = os.path.join(DATASET_DIR, "images", f'{next_img_nr}.png')
+            output_path_skel = os.path.join(DATASET_DIR, "conditioning_images", f'{next_img_nr}.png')
+            input.save(output_path)
+            imsave(output_path_skel, img_as_ubyte(skeleton), check_contrast=False)
+
+            next_img_nr += 1
+        except Exception as error:
+            print(f'An error occurred in image {img_path}: {error}')
+        
+    create_jsonl_data_file(DATASET_DIR)
+
+    nr_of_images = len(os.listdir(DS_IMAGES_DIR))
+    print(f'Dataset created with {nr_of_images} images')
+
+
+def create_jsonl_data_file(DATASET_DIR):
+    # Create jsonl data file with text (prompt), image and conditioning image (skeleton) 
     nr_of_images = len(os.listdir(os.path.join(DATASET_DIR, "images")))
     metadata = [{"text": "", "image": f"images/{i}.png", "conditioning_image": f"conditioning_images/{i}.png"} for i in range(nr_of_images)]
 
